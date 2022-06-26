acc67d7e0884dffdd8b09c5348cd77cba061958a
MESUT BIDAK <mesut.bidak@bahcesehir.edu.tr>
2022-06-26 15:25:38 +0300

    added comments into post detail page

--- src/Screens/PostDetailScreen.js
+++ src/Screens/PostDetailScreen.js
@@ -1,9 +1,13 @@
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, TouchableOpacity, ScrollView } from 'react-native';
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Card } from 'react-native-elements';
+
 //import * as React from 'react';
 // import { NavigationContainer } from '@react-navigation/native';
 // import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 
 const PostDetailScreen = ({ route, navigation }) => {
     const apiUrlPosts = "https://jsonplaceholder.typicode.com/posts";
@@ -12,6 +16,7 @@ const PostDetailScreen = ({ route, navigation }) => {
     const { postId } = route.params;
     const [post, setPost] = useState({});
     const [user, setUser] = useState({});
+    const [comments, setComments] = useState([]);
 
     useEffect(() => {
         fetch(apiUrlPosts + "/" + postId)
@@ -28,56 +33,60 @@ const PostDetailScreen = ({ route, navigation }) => {
                         setUser(data);
                     });
             })
+
+        //https://jsonplaceholder.typicode.com/posts/3/comments
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+            .then((res) => {
+                setComments(res.data);
+                //console.log(res.data);
+            })
+
     }, []);
 
     return (
-        <View style={styles.container}>
-            <View style={styles.postView}>
-                <Text style={styles.postBoldText}>User Name : {user.name}</Text>
-                <Text style={styles.postText}>Title : {post.title}</Text>
-            </View>
-            <View style={styles.postView}>
-                <Text style={styles.postBoldText}>BODY</Text>
-                <Text style={styles.postText}>{post.body}</Text>
-            </View>
+        <View>
+            <ScrollView>
+                <Card>
+                    <Text>
+                        <Text style={{ fontWeight: "bold" }}> User  Name : </Text>
+                        <Text>{user.name}</Text>
+                    </Text>
+                    <Text>
+                        <Text style={{ fontWeight: "bold" }}> Title : </Text>
+                        <Text>{post.title}</Text>
+                    </Text>
+                    <Text>
+                        <Text style={{ fontWeight: "bold" }}> Content : </Text>
+                        <Text>{post.body}</Text></Text>
+                </Card>
+                <Card>
+                    <View style={{ alignContent: "center", alignItems: "center" }}>
+                        <Text style={{ fontWeight: "bold" }}>COMMENTS</Text>
+                    </View>
+                </Card>
+                {
+                    comments.map((item) => {
+                        return (
+                            <Card key={item.id}>
+                                <Text style={{ marginBottom: 10 }}>
+                                    <Text style={{ fontWeight: "bold" }}> User  Name : </Text>
+                                    <Text>{item.name}</Text>
+                                </Text>
+                                <Text style={{ marginBottom: 10 }}>
+                                    <Text style={{ fontWeight: "bold" }}> Email : </Text>
+                                    <Text>{item.email}</Text>
+                                </Text>
+                                <Text style={{ marginBottom: 10 }}>
+                                    <Text style={{ fontWeight: "bold" }}> Body : </Text>
+                                    <Text>{item.body}</Text>
+                                </Text>
+                            </Card>
+                        )
+                    })
+                }
+            </ScrollView>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1, //the container will fill the whole screen.
-        //justifyContent: "flex-end",
-        alignItems: "center",
-    },
-    postView: {
-        //flex: 1,
-        padding: 3,
-        margin: 5,
-        alignItems: 'center',
-        //justifyContent: 'center',
-        borderRadius: 10,
-        elevation: 3,
-        backgroundColor: '#fff',
-        shadowOffset: { width: 1, height: 1 },
-        shadowColor: '#333',
-        shadowOpacity: 0.3,
-        shadowRadius: 2,
-    },
-    postText: {
-        fontSize: 20,
-        fontStyle: 'normal',
-        fontFamily: "Cochin",
-        fontWeight: 'normal',
-        color: 'gray',
-    },
-    postBoldText:{
-        fontSize: 20,
-        fontStyle: 'normal',
-        fontFamily: "Cochin",
-        fontWeight: 'bold',
-        color: 'gray',
-    },
-});
 
 export default PostDetailScreen
